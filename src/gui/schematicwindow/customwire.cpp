3d52b7a1cd5be7b80bea0a0a95dde2b837628377
Huang Rui <vowstar@gmail.com>
2025-10-30 16:56:59 +0800

    feat(schematic): Add custom wire with bus visualization support
    - Implement CustomWire class with bus wire flag and serialization
    - Add bus detection logic in netlist generation for auto-styling
    - Register custom wire factory in schematic scene for bus rendering
    
    Signed-off-by: Huang Rui <vowstar@gmail.com>

---
+++ src/gui/schematicwindow/customwire.cpp
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-2025 Huang Rui <vowstar@gmail.com>
+
+#include "customwire.h"
+
+#include <QPainter>
+
+using namespace SchematicCustom;
+
+CustomWire::CustomWire(int type, QGraphicsItem *parent)
+    : QSchematic::Items::Wire(type, parent)
+{}
+
+bool CustomWire::isBusWire() const
+{
+    return m_isBusWire;
+}
+
+void CustomWire::setBusWire(bool isBus)
+{
+    m_isBusWire = isBus;
+}
+
+gpds::container CustomWire::to_container() const
+{
+    auto container = Wire::to_container();
+    container.add_value("is_bus", m_isBusWire);
+    return container;
+}
+
+void CustomWire::from_container(const gpds::container &container)
+{
+    Wire::from_container(container);
+    m_isBusWire = container.get_value<bool>("is_bus").value_or(false);
+}
+
+void CustomWire::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    /* If this is a bus wire, draw thicker background line first */
+    if (isBusWire()) {
+        QPen busPen;
+        busPen.setStyle(Qt::SolidLine);
+        busPen.setCapStyle(Qt::RoundCap);
+        busPen.setWidth(5);                          // Thicker than normal (normal is 1)
+        busPen.setColor(QColor(100, 130, 200, 160)); // Soft deep blue, semi-transparent
+
+        painter->setPen(busPen);
+        painter->setBrush(Qt::NoBrush);
+
+        /* Draw the background polyline */
+        const auto &points = pointsRelative();
+        painter->drawPolyline(points.constData(), points.count());
+    }
+
+    /* Call base class to draw normal wire on top */
+    QSchematic::Items::Wire::paint(painter, option, widget);
+}
